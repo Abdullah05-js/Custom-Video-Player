ead44540f645ea2aacc60403608f8c06126fdbad
thodex <abdallhameed2005@gmail.com>
2025-05-12 00:56:38 +0300

    feat:add hls support and new style

--- Video.js
+++ Video.js
@@ -1,104 +1,184 @@
-import React,{useState,useRef} from 'react';
-
-import { Button } from '@nextui-org/button';
-import { MutedSpeakerIcon, PlayIcon, SpeakerIcon,PictureInPictureIcon, FullScreenIcon, ResSizeScreenIcon } from '../../svgs/Icon';
-import { PauseIcon } from '../../svgs/Icon';
-import { Chip } from '@nextui-org/chip';
-
+import { useState, useRef, useEffect } from 'react';
+import Hls from "hls.js"
+import { Button } from '@heroui/button';
+import { MutedSpeakerIcon, ReLoadIcom, PlayIcon, SpeakerIcon, PictureInPictureIcon, FullScreenIcon, ResSizeScreenIcon } from "./svgs/icon.js";
+import { PauseIcon } from './svgs/icon.js';
 const handleDuration = (time) => {
 
-    const format = new Intl.NumberFormat(undefined,{
-        minimumIntegerDigits:2
+    const format = new Intl.NumberFormat(undefined, {
+        minimumIntegerDigits: 2
     })
-   const m =  Math.floor(time/60) % 60
-   const s = Math.floor(time%60)
-   const h =  Math.floor(time /3600)
-   if(h === 0)
-    return `${m}:${format.format(s)}`;
-   else
-    return `${h}:${format.format(h)}:${format.format(s)}`;
+    const m = Math.floor(time / 60) % 60
+    const s = Math.floor(time % 60)
+    const h = Math.floor(time / 3600)
+    if (h === 0)
+        return `${m}:${format.format(s)}`;
+    else
+        return `${h}:${format.format(h)}:${format.format(s)}`;
 }
 
-const Video = ({SRC}) => {
-    const VideoRef = useRef();
-    const VideoContinerRef = useRef();
-    const [Play,setPlay] = useState(false);
-    const [isMuted,setisMuted] = useState(false);
-    const [ShowSlider,SetShowSlider] = useState(false);
-    const [isFullScreen,SetisFullScreen] = useState(false);
-    const [VideoDuration,setVideoDuration] = useState(0);
-    const [MainTimeLine,setMainTimeLine] =useState(handleDuration(0));
+const isHls = (link) => {
+    if (link.includes(".m3u8") || link.includes(".ts")) return true;
+    return false
+}
 
+const Video = ({ SRC }) => {
+    const VideoRef = useRef();
+
+    const VideoContinerRef = useRef();
+    const [Play, setPlay] = useState(false);
+    const [isMuted, setisMuted] = useState(false);
+    const [hls, setHls] = useState();
+    const [isFullScreen, SetisFullScreen] = useState(false);
+    const [VideoDuration, setVideoDuration] = useState(0);
+    const [MainTimeLine, setMainTimeLine] = useState(handleDuration(0));
+    const [HideHeader, setHideHeader] = useState(false)
+
+
+    useEffect(() => {
+        let localHls;
+
+        if (isHls(SRC)) {
+
+            if (VideoRef.current.canPlayType('application/vnd.apple.mpegurl')) {
+                VideoRef.current.src = SRC
+            }
+            else if (Hls.isSupported()) {
+                localHls = new Hls({
+                    "debug": true
+                });
+
+                localHls.log = true;
+                localHls.loadSource(SRC);
+                localHls.attachMedia(VideoRef.current)
+                localHls.on(Hls.Events.ERROR, (err) => {
+                    console.log(err)
+                });
+                localHls.on(Hls.Events.MANIFEST_PARSED, () => {
+                    VideoRef.current.play()
+                    setPlay(true)
+                });
+                setHls(localHls)
+
+            } else {
+                console.log('hls not supported')
+                alert("hls not supported")
+            }
+
+        } else {
+            VideoRef.current.src = SRC
+        }
+        return () => {
+            if (localHls) {
+                localHls.destroy();
+            }
+        };
+
+    }, [SRC])
 
     const handleTimeUpdate = () => {
         setMainTimeLine(handleDuration(VideoRef.current.currentTime))
     }
 
     const handlePictureInPicture = () => {
-        
+
         if (document.pictureInPictureElement) {
             document.exitPictureInPicture();
-          } else if (document.pictureInPictureEnabled) {
+        } else if (document.pictureInPictureEnabled) {
             handPlay();
             VideoRef.current.requestPictureInPicture();
-          }
+        }
 
     }
 
     const handMute = () => {
-         setisMuted(e => !e)
-        
-         isMuted ? VideoRef.current.muted = false : VideoRef.current.muted = true;
+        setisMuted(e => {
+            VideoRef.current.muted = !e
+            return !e
+        })
     }
 
     const handPlay = () => {
-       setPlay(e => !e)
-       
-       Play ? VideoRef.current.pause() : VideoRef.current.play();
+        setPlay(prev => {
+            const newState = !prev;
+            if (newState) {
+                VideoRef.current.play();
+            } else {
+                VideoRef.current.pause();
+            }
+            return newState;
+        });
     }
 
     const handFullScreen = () => {
         SetisFullScreen((e) => !e)
-
-        isFullScreen ? document.exitFullscreen():VideoContinerRef.current.requestFullscreen();        
-        
+        isFullScreen ? setHideHeader(!isFullScreen) : setHideHeader(!isFullScreen)
+        isFullScreen ? document.exitFullscreen() : VideoContinerRef.current.requestFullscreen();
     }
-  
+
+    const handReload = () => {
+        setPlay(false)
+        if (hls) {
+            const newHls = new Hls();
+
+            newHls.loadSource(SRC);
+            newHls.attachMedia(VideoRef.current);
+
+            newHls.on(Hls.Events.MANIFEST_PARSED, () => {
+                VideoRef.current.currentTime = 0;
+                VideoRef.current.play();
+                setPlay(true);
+            });
+
+            newHls.on(Hls.Events.ERROR, (event, data) => {
+                console.error("HLS reload error", data);
+            });
+
+            setHls(newHls);
+        }
+        else {
+            VideoRef.current.load()
+        }
+    }
+
     return (
-        <div ref={VideoContinerRef} onClick={handPlay} onDoubleClick={handFullScreen} className={`text-primary ss:w-full sm:w-full max-w-[672px] relative rounded-3xl ${isFullScreen ? "w-screen h-screen" : ""}`}>
-             <div className='flex flex-row justify-between items-center absolute bottom-0 left-0 right-0 z-50 p-2'>
-                <div className='flex flex-row justify-center items-center'>   
-                    <Button isIconOnly onClick={handPlay} className='bg-transparent  ml-2 flex  justify-center items-center'>
-                      { Play ?   <PauseIcon/> : <PlayIcon/>}
+        <div ref={VideoContinerRef} onClick={handPlay} onDoubleClick={handFullScreen} className={`text-primary group  w-full h-full relative rounded-3xl ${isFullScreen ? "w-screen h-screen" : ""} shadow-green-300 shadow-md `}>
+            <div className={`z-50 -translate-y-6 absolute invisible group-hover:visible  p-2 flex flex-row justify-center items-center bg-opacity-35 bg-black w-1/3 m-1 transform  group-hover:translate-y-1 transition duration-500 ease-in-out rounded-3xl ${HideHeader && "hidden"}`} color='success'>
+                Powered by THODEX.live
+            </div>
+            <div className='flex flex-row justify-between bg-black bg-opacity-30 translate-y-6 items-center absolute invisible group-hover:visible transform  group-hover:translate-y-1 transition duration-500 ease-in-out bottom-0 left-0 right-0 z-50 p-2 rounded-xl'>
+                <div className='flex flex-row justify-center items-center'>
+                    <Button isIconOnly onPress={handPlay} className='bg-transparent  ml-2 flex  justify-center items-center'>
+                        {Play ? <PauseIcon /> : <PlayIcon />}
                     </Button>
 
-                    <Chip className='bg-transparent text-primary text-center pb-1'>
-                        Powered by Gamblers
-                    </Chip>
+                    {!hls && <p className='flex  justify-center items-center'>{`${MainTimeLine}/${handleDuration(VideoDuration)}`}</p>}
 
                 </div>
-                
-                <div className='flex flex-row gap-7 justify-center items-center'>
-                <p className='flex  justify-center items-center'>{`${MainTimeLine}/${handleDuration(VideoDuration)}`}</p>
 
-              
-                <Button isIconOnly onClick={handMute}  className='bg-transparent flex  justify-center items-center'>
-                  {isMuted ? <MutedSpeakerIcon /> : <SpeakerIcon/>}
-                 
-                </Button>
-           
-                
-                <Button isIconOnly onClick={handlePictureInPicture} className='bg-transparent text-primary flex  justify-center items-center'>
-                   <PictureInPictureIcon/>
-                </Button>
-                <Button isIconOnly onClick={handFullScreen} className='bg-transparent text-primary mr-2 flex  justify-center items-center'>
-                   { isFullScreen ?  <ResSizeScreenIcon/> :<FullScreenIcon /> }
-                  
-                </Button>
+                <div className='flex flex-row max-sm:gap-0 gap-7 justify-center items-center'>
+
+                    <Button onPress={handReload} isIconOnly color='success' className='bg-transparent'>
+                        <ReLoadIcom />
+                    </Button>
+
+                    <Button isIconOnly onPress={handMute} className='bg-transparent flex  justify-center items-center'>
+                        {isMuted ? <MutedSpeakerIcon /> : <SpeakerIcon />}
+
+                    </Button>
+
+
+                    <Button isIconOnly onPress={handlePictureInPicture} className='bg-transparent text-primary flex  justify-center items-center'>
+                        <PictureInPictureIcon />
+                    </Button>
+                    <Button isIconOnly onPress={handFullScreen} className='bg-transparent text-primary mr-2 flex  justify-center items-center'>
+                        {isFullScreen ? <ResSizeScreenIcon /> : <FullScreenIcon />}
+
+                    </Button>
                 </div>
             </div>
-                {/* here we used oncanplay to avoid error when the media is not loaded bc if its not the duration will be undifend */}
-             <video loop  onTimeUpdate={handleTimeUpdate} onCanPlay={() => setVideoDuration(VideoRef.current.duration)} ref={VideoRef} src={SRC} className={` object-cover rounded-3xl ${isFullScreen ? "w-screen h-screen" : ""}`}></video>
+            {/* here we used oncanplay to avoid error when the media is not loaded bc if its not the duration will be undifend */}
+            <video loop onTimeUpdate={handleTimeUpdate} onCanPlay={() => setVideoDuration(VideoRef.current.duration)} ref={VideoRef} className={` object-cover rounded-3xl w-full h-full ${isFullScreen ? "w-screen h-screen" : ""}`}></video>
         </div>
     );
 }
--- svgs/icon.js
+++ svgs/icon.js
@@ -1,57 +1,57 @@
 import React from 'react'
 
-const Svg = ({width=48,height=48,children}) => {
+const Svg = ({ width = 48, height = 48, children }) => {
   return (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"  width={width} height={height}>
-        {children}
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
+      {children}
     </svg>
   )
 }
 
-export const PlayIcon = ({width=38,height=38}) => {
-  return( 
+export const PlayIcon = ({ width = 38, height = 38 }) => {
+  return (
     <Svg width={width} height={height} className="bg-transparent">
-      <svg  viewBox="0 0 24 24"  ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <rect width="24" height="24" ></rect> <path d="M16.7519 11.1679L10.5547 7.03647C9.89015 6.59343 9 7.06982 9 7.86852V16.1315C9 16.9302 9.89015 17.4066 10.5547 16.9635L16.7519 12.8321C17.3457 12.4362 17.3457 11.5638 16.7519 11.1679Z" stroke="#00ff9d" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+      <svg viewBox="0 0 24 24"  ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <rect width="24" height="24" ></rect> <path d="M16.7519 11.1679L10.5547 7.03647C9.89015 6.59343 9 7.06982 9 7.86852V16.1315C9 16.9302 9.89015 17.4066 10.5547 16.9635L16.7519 12.8321C17.3457 12.4362 17.3457 11.5638 16.7519 11.1679Z" stroke="#00ff9d" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
     </Svg>
   )
 }
 
 
-export const PauseIcon = ({width=28,height=28}) => {
-  return( 
+export const PauseIcon = ({ width = 28, height = 28 }) => {
+  return (
     <Svg width={width} height={height} className="bg-transparent">
-      <svg  viewBox="0 0 24 24" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#86efac" d="M10,0 C15.5228475,0 20,4.4771525 20,10 C20,15.5228475 15.5228475,20 10,20 C4.4771525,20 0,15.5228475 0,10 C0,4.4771525 4.4771525,0 10,0 Z M10,1.39534884 C5.24778239,1.39534884 1.39534884,5.24778239 1.39534884,10 C1.39534884,14.7522176 5.24778239,18.6046512 10,18.6046512 C14.7522176,18.6046512 18.6046512,14.7522176 18.6046512,10 C18.6046512,5.24778239 14.7522176,1.39534884 10,1.39534884 Z M8.05611421,5.45454545 C8.43267199,5.45454545 8.73793239,5.75980585 8.73793239,6.13636364 L8.73793239,13.4084582 C8.73793239,13.7850159 8.43267199,14.0902763 8.05611421,14.0902763 C7.67955643,14.0902763 7.37429603,13.7850159 7.37429603,13.4084582 L7.37429603,6.13636364 C7.37429603,5.75980585 7.67955643,5.45454545 8.05611421,5.45454545 Z M12.6015688,5.45454545 C12.9781265,5.45454545 13.2833869,5.75980585 13.2833869,6.13636364 L13.2833869,13.4084582 C13.2833869,13.7850159 12.9781265,14.0902763 12.6015688,14.0902763 C12.225011,14.0902763 11.9197506,13.7850159 11.9197506,13.4084582 L11.9197506,6.13636364 C11.9197506,5.75980585 12.225011,5.45454545 12.6015688,5.45454545 Z"></path> </g></svg>
+      <svg viewBox="0 0 24 24" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill="#86efac" d="M10,0 C15.5228475,0 20,4.4771525 20,10 C20,15.5228475 15.5228475,20 10,20 C4.4771525,20 0,15.5228475 0,10 C0,4.4771525 4.4771525,0 10,0 Z M10,1.39534884 C5.24778239,1.39534884 1.39534884,5.24778239 1.39534884,10 C1.39534884,14.7522176 5.24778239,18.6046512 10,18.6046512 C14.7522176,18.6046512 18.6046512,14.7522176 18.6046512,10 C18.6046512,5.24778239 14.7522176,1.39534884 10,1.39534884 Z M8.05611421,5.45454545 C8.43267199,5.45454545 8.73793239,5.75980585 8.73793239,6.13636364 L8.73793239,13.4084582 C8.73793239,13.7850159 8.43267199,14.0902763 8.05611421,14.0902763 C7.67955643,14.0902763 7.37429603,13.7850159 7.37429603,13.4084582 L7.37429603,6.13636364 C7.37429603,5.75980585 7.67955643,5.45454545 8.05611421,5.45454545 Z M12.6015688,5.45454545 C12.9781265,5.45454545 13.2833869,5.75980585 13.2833869,6.13636364 L13.2833869,13.4084582 C13.2833869,13.7850159 12.9781265,14.0902763 12.6015688,14.0902763 C12.225011,14.0902763 11.9197506,13.7850159 11.9197506,13.4084582 L11.9197506,6.13636364 C11.9197506,5.75980585 12.225011,5.45454545 12.6015688,5.45454545 Z"></path> </g></svg>
     </Svg>
   )
 }
 
-export const SpeakerIcon = ({width=28,height=28}) => {
-  return( 
+export const SpeakerIcon = ({ width = 28, height = 28 }) => {
+  return (
     <Svg width={width} height={height} className="bg-transparent">
-      <svg  viewBox="0 0 24 24" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 6C20.5 7.5 21 10 21 12C21 14 20.5 16.5 19 18M16 8.99998C16.5 9.49998 17 10.5 17 12C17 13.5 16.5 14.5 16 15M3 10.5V13.5C3 14.6046 3.5 15.5 5.5 16C7.5 16.5 9 21 12 21C14 21 14 3 12 3C9 3 7.5 7.5 5.5 8C3.5 8.5 3 9.39543 3 10.5Z" stroke="#86efac" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+      <svg viewBox="0 0 24 24" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M19 6C20.5 7.5 21 10 21 12C21 14 20.5 16.5 19 18M16 8.99998C16.5 9.49998 17 10.5 17 12C17 13.5 16.5 14.5 16 15M3 10.5V13.5C3 14.6046 3.5 15.5 5.5 16C7.5 16.5 9 21 12 21C14 21 14 3 12 3C9 3 7.5 7.5 5.5 8C3.5 8.5 3 9.39543 3 10.5Z" stroke="#86efac" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
     </Svg>
   )
 }
 
-export const MutedSpeakerIcon = ({width=28,height=28}) => {
-  return( 
+export const MutedSpeakerIcon = ({ width = 28, height = 28 }) => {
+  return (
     <Svg width={width} height={height} className="bg-transparent">
-      <svg  viewBox="0 0 24 24" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5 8.14307C3.4148 8.66137 3 9.49393 3 10.5V13.5C3 14.6046 3.5 15.5 5.5 16C7.5 16.5 9 21 12 21C12.6098 21 13.0337 19.3265 13.2717 17M3 3L21 21M9 4.60756C9.84604 3.71548 10.8038 3 12 3C12.7739 3 13.2484 5.69533 13.4233 9" stroke="#86efac" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+      <svg viewBox="0 0 24 24" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M5 8.14307C3.4148 8.66137 3 9.49393 3 10.5V13.5C3 14.6046 3.5 15.5 5.5 16C7.5 16.5 9 21 12 21C12.6098 21 13.0337 19.3265 13.2717 17M3 3L21 21M9 4.60756C9.84604 3.71548 10.8038 3 12 3C12.7739 3 13.2484 5.69533 13.4233 9" stroke="#86efac" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
     </Svg>
   )
 }
 
 
-export const  PictureInPictureIcon = ({width=28,height=28}) => {
-  return( 
+export const PictureInPictureIcon = ({ width = 28, height = 28 }) => {
+  return (
     <Svg width={width} height={height} className="bg-transparent">
       <svg viewBox="0 0 24 24" fill="#86efac"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path fill="none" d="M0 0h24v24H0z"></path> <path fill-rule="nonzero" d="M21 3a1 1 0 0 1 1 1v7h-2V5H4v14h6v2H3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h18zm0 10a1 1 0 0 1 1 1v6a1 1 0 0 1-1 1h-8a1 1 0 0 1-1-1v-6a1 1 0 0 1 1-1h8zm-1 2h-6v4h6v-4zm-8.5-8L9.457 9.043l2.25 2.25-1.414 1.414-2.25-2.25L6 12.5V7h5.5z"></path> </g> </g></svg>
     </Svg>
   )
 }
 
-export const  FullScreenIcon = ({width=22,height=22}) => {
-  return( 
+export const FullScreenIcon = ({ width = 22, height = 22 }) => {
+  return (
     <Svg width={width} height={height} className="bg-transparent">
       <svg fill="#6ee7b7" viewBox="0 0 1920 1920"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M1146.616-.012V232.38h376.821L232.391 1523.309v-376.705H0V1920h773.629v-232.39H396.69L1687.737 396.68V773.5h232.275V-.011z" fill-rule="evenodd"></path> </g></svg>
     </Svg>
@@ -59,11 +59,20 @@ export const  FullScreenIcon = ({width=22,height=22}) => {
 }
 
 
-export const  ResSizeScreenIcon = ({width=30,height=30}) => {
-  return( 
+export const ResSizeScreenIcon = ({ width = 30, height = 30 }) => {
+  return (
     <Svg width={width} height={height} className="bg-transparent">
-      <svg  fill="#86efac" viewBox="0 0 36 36" version="1.1" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>resize-down-line</title> <path class="clr-i-outline clr-i-outline-path-1" d="M32,15H22.41l9.25-9.25a1,1,0,0,0-1.41-1.41L21,13.59V4a1,1,0,0,0-2,0V17H32a1,1,0,0,0,0-2Z"></path><path  d="M4,19a1,1,0,0,0,0,2h9.59L4.33,30.25a1,1,0,1,0,1.41,1.41L15,22.41V32a1,1,0,0,0,2,0V19Z"></path> </g></svg>
- </Svg>
+      <svg fill="#86efac" viewBox="0 0 36 36" version="1.1" ><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>resize-down-line</title> <path class="clr-i-outline clr-i-outline-path-1" d="M32,15H22.41l9.25-9.25a1,1,0,0,0-1.41-1.41L21,13.59V4a1,1,0,0,0-2,0V17H32a1,1,0,0,0,0-2Z"></path><path d="M4,19a1,1,0,0,0,0,2h9.59L4.33,30.25a1,1,0,1,0,1.41,1.41L15,22.41V32a1,1,0,0,0,2,0V19Z"></path> </g></svg>
+    </Svg>
+  )
+}
+
+
+export const ReLoadIcom = ({ width = 30, height = 30 }) => {
+  return (
+    <Svg width={width} height={height} className="bg-transparent">
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4.39502 12.0014C4.39544 12.4156 4.73156 12.751 5.14577 12.7506C5.55998 12.7502 5.89544 12.4141 5.89502 11.9999L4.39502 12.0014ZM6.28902 8.1116L6.91916 8.51834L6.91952 8.51777L6.28902 8.1116ZM9.33502 5.5336L9.0396 4.84424L9.03866 4.84464L9.33502 5.5336ZM13.256 5.1336L13.4085 4.39927L13.4062 4.39878L13.256 5.1336ZM16.73 7.0506L16.1901 7.57114L16.1907 7.57175L16.73 7.0506ZM17.7142 10.2078C17.8286 10.6059 18.2441 10.8358 18.6422 10.7214C19.0403 10.607 19.2703 10.1915 19.1558 9.79342L17.7142 10.2078ZM17.7091 9.81196C17.6049 10.2129 17.8455 10.6223 18.2464 10.7265C18.6473 10.8307 19.0567 10.5901 19.1609 10.1892L17.7091 9.81196ZM19.8709 7.45725C19.9751 7.05635 19.7346 6.6469 19.3337 6.54272C18.9328 6.43853 18.5233 6.67906 18.4191 7.07996L19.8709 7.45725ZM18.2353 10.7235C18.6345 10.8338 19.0476 10.5996 19.1579 10.2004C19.2683 9.80111 19.034 9.38802 18.6348 9.2777L18.2353 10.7235ZM15.9858 8.5457C15.5865 8.43537 15.1734 8.66959 15.0631 9.06884C14.9528 9.46809 15.187 9.88119 15.5863 9.99151L15.9858 8.5457ZM19.895 11.9999C19.8946 11.5856 19.5585 11.2502 19.1443 11.2506C18.7301 11.251 18.3946 11.5871 18.395 12.0014L19.895 11.9999ZM18.001 15.8896L17.3709 15.4829L17.3705 15.4834L18.001 15.8896ZM14.955 18.4676L15.2505 19.157L15.2514 19.1566L14.955 18.4676ZM11.034 18.8676L10.8815 19.6019L10.8839 19.6024L11.034 18.8676ZM7.56002 16.9506L8.09997 16.4301L8.09938 16.4295L7.56002 16.9506ZM6.57584 13.7934C6.46141 13.3953 6.04593 13.1654 5.64784 13.2798C5.24974 13.3942 5.01978 13.8097 5.13421 14.2078L6.57584 13.7934ZM6.58091 14.1892C6.6851 13.7884 6.44457 13.3789 6.04367 13.2747C5.64277 13.1705 5.23332 13.4111 5.12914 13.812L6.58091 14.1892ZM4.41914 16.544C4.31495 16.9449 4.55548 17.3543 4.95638 17.4585C5.35727 17.5627 5.76672 17.3221 5.87091 16.9212L4.41914 16.544ZM6.05478 13.2777C5.65553 13.1674 5.24244 13.4016 5.13212 13.8008C5.02179 14.2001 5.25601 14.6132 5.65526 14.7235L6.05478 13.2777ZM8.30426 15.4555C8.70351 15.5658 9.11661 15.3316 9.22693 14.9324C9.33726 14.5331 9.10304 14.12 8.70378 14.0097L8.30426 15.4555ZM5.89502 11.9999C5.89379 10.7649 6.24943 9.55591 6.91916 8.51834L5.65889 7.70487C4.83239 8.98532 4.3935 10.4773 4.39502 12.0014L5.89502 11.9999ZM6.91952 8.51777C7.57513 7.50005 8.51931 6.70094 9.63139 6.22256L9.03866 4.84464C7.65253 5.4409 6.47568 6.43693 5.65852 7.70544L6.91952 8.51777ZM9.63045 6.22297C10.7258 5.75356 11.9383 5.62986 13.1059 5.86842L13.4062 4.39878C11.9392 4.09906 10.4158 4.25448 9.0396 4.84424L9.63045 6.22297ZM13.1035 5.86793C14.2803 6.11232 15.3559 6.7059 16.1901 7.57114L17.27 6.53006C16.2264 5.44761 14.8807 4.70502 13.4085 4.39927L13.1035 5.86793ZM16.1907 7.57175C16.9065 8.31258 17.4296 9.21772 17.7142 10.2078L19.1558 9.79342C18.8035 8.5675 18.1557 7.44675 17.2694 6.52945L16.1907 7.57175ZM19.1609 10.1892L19.8709 7.45725L18.4191 7.07996L17.7091 9.81196L19.1609 10.1892ZM18.6348 9.2777L15.9858 8.5457L15.5863 9.99151L18.2353 10.7235L18.6348 9.2777ZM18.395 12.0014C18.3963 13.2363 18.0406 14.4453 17.3709 15.4829L18.6312 16.2963C19.4577 15.0159 19.8965 13.5239 19.895 11.9999L18.395 12.0014ZM17.3705 15.4834C16.7149 16.5012 15.7707 17.3003 14.6587 17.7786L15.2514 19.1566C16.6375 18.5603 17.8144 17.5643 18.6315 16.2958L17.3705 15.4834ZM14.6596 17.7782C13.5643 18.2476 12.3517 18.3713 11.1842 18.1328L10.8839 19.6024C12.3508 19.9021 13.8743 19.7467 15.2505 19.157L14.6596 17.7782ZM11.1865 18.1333C10.0098 17.8889 8.93411 17.2953 8.09997 16.4301L7.02008 17.4711C8.06363 18.5536 9.40936 19.2962 10.8815 19.6019L11.1865 18.1333ZM8.09938 16.4295C7.38355 15.6886 6.86042 14.7835 6.57584 13.7934L5.13421 14.2078C5.48658 15.4337 6.13433 16.5545 7.02067 17.4718L8.09938 16.4295ZM5.12914 13.812L4.41914 16.544L5.87091 16.9212L6.58091 14.1892L5.12914 13.812ZM5.65526 14.7235L8.30426 15.4555L8.70378 14.0097L6.05478 13.2777L5.65526 14.7235Z" fill="#86efac"></path> </g></svg>
+    </Svg>
   )
 }
 
